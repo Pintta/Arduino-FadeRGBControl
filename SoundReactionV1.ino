e7dcc3ac801c640fbe0d176dcf59db76567f0c82
Pintta <69728770+Pintta@users.noreply.github.com>
2023-01-17 18:28:27 +0200

    Update SoundReactionV1.ino

--- SoundReactionV1.ino
+++ SoundReactionV1.ino
@@ -1,6 +1,7 @@
 // MODE
 int fadeAmount = 3;  // How many checkpoints for fade, this 3 is perfect default, but if you want use 5 :)
 int k = 0; // Don't touch them
+int xaxa = digitalRead(xac);
 // PIN
 int xac = 13; // ACTIVATED SENSOR // PIN 13
 int xp = 7; // Red (R) // PIN 7
@@ -22,14 +23,11 @@ void loop() {
   if (k <= 0 || k >= 125) { // 125 is default brightness value and maxium is 255, minium is 0 :)
     fadeAmount = -fadeAmount;
   }
-  int xaxa = digitalRead(xac);
   delay(100); // DELAY MEAN TIME MS, DEFAULT 100MS = 0,1 SECONDS
-    if (xaxa == 1) // If you have sensor, this activated
-  {
+  if (xaxa == 1) {// If you have sensor, this activated
     analogWrite(xp, HIGH); // Activated sensor
   }
-  else // Not resposive sensor
-  {
+  else { // Not resposive sensor
     digitalWrite(xp, LOW); // No activated sensor
   }
 }
